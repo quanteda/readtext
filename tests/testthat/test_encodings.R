7e3bd385d1cb23383258c5c9498975a6a94b5600
Adam Obeng <github@binaryeagle.com>
2016-11-02 15:33:27 +0000

    Add test armature
    Signed-off-by: Adam Obeng <github@binaryeagle.com>

--- tests/testthat/test_encodings.R
+++ tests/testthat/test_encodings.R
@@ -1,18 +1,24 @@
 context('test encodings.R')
 
 
-FILEDIR <- '../data/encoding'
+test_that("test if encoding() detects encodings correctly", {
+	  #     skip_on_cran()
+	  #     skip_on_travis()
+	FILEDIR <- '../data/encoding'
 
-filenames <- list.files(FILEDIR, "*__characters.txt$")
-parts <- strsplit(gsub(".txt$", "", filenames), "__")
-fileencodings <- sapply(parts, "[", 1)
+	filenames <- list.files(FILEDIR, "*__characters.txt$")
+	parts <- strsplit(gsub(".txt$", "", filenames), "__")
+	fileencodings <- sapply(parts, "[", 1)
 
-fileencodings <- fileencodings[!(fileencodings %in% broken_encodings)]
-filenames <- file.path(FILEDIR, paste0(fileencodings,  "__characters.txt"))
+	filenames <- file.path(FILEDIR, paste0(fileencodings,  "__characters.txt"))
 
-for (i in 1:length(fileencodings)) {
-	filename <- filenames[[i]]
-	detected_encoding <- encoding2(filename)$probably
-	actual_encoding <- fileencodings[[i]]
-	expect_equal(detected_encoding, actual_encoding)
-}
+	for (i in 1:length(fileencodings)) {
+		filename <- filenames[[i]]
+		detected_encoding <- encoding2(filename)$probably
+		actual_encoding <- fileencodings[[i]]
+		test_that(paste("test textfile encoding parameter, encoding", actual_encoding), {
+			expect_equal(detected_encoding, actual_encoding)
+		})
+	}
+
+})
