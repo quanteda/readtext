d229f2f2b2f2780d3e5f431a24cbcdd0640309e5
Kenneth Benoit <kbenoit@lse.ac.uk>
2016-11-17 17:47:29 -0500

    Add tests for file_ext and getdocvarsFromFilenames

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,16 +1,16 @@
-test_that("Test quanteda:::mktemp function for test dirs",{
-    filename <- quanteda:::mktemp()
+test_that("Test readtext:::mktemp function for test dirs",{
+    filename <- readtext:::mktemp()
     expect_true(file.exists(filename))
-    filename2 <- quanteda:::mktemp()
+    filename2 <- readtext:::mktemp()
     expect_true(file.exists(filename2))
     expect_false(filename == filename2)
     
     # test directory parameter
-    dirname <- quanteda:::mktemp(directory=T)
+    dirname <- readtext:::mktemp(directory=T)
     expect_true(dir.exists(dirname))
     
     # test prefix parameter
-    filename <- quanteda:::mktemp(prefix='testprefix')
+    filename <- readtext:::mktemp(prefix='testprefix')
     expect_equal(
         substr(basename(filename), 1, 10),
         'testprefix'
@@ -18,9 +18,9 @@ test_that("Test quanteda:::mktemp function for test dirs",{
     
     # test that a new filename will be given if the original already exists
     set.seed(0)
-    original_filename <- quanteda:::mktemp()
+    original_filename <- readtext:::mktemp()
     set.seed(0)
-    new_filename <- quanteda:::mktemp()
+    new_filename <- readtext:::mktemp()
     expect_false(original_filename == new_filename)
     expect_true(file.exists(original_filename))
     expect_true(file.exists(new_filename))
@@ -28,3 +28,29 @@ test_that("Test quanteda:::mktemp function for test dirs",{
     
 })
 
+
+test_that("Test readtext:::getdocvarsFromFilenames for parsing filenames", {
+    
+    filenames <- c("~/tmp/documents/USA_blue_horse.txt",
+                   "~/tmp/documents/France_green_dog.txt",
+                   "~/tmp/documents/China_red_dragon.txt",
+                   "~/tmp/spaced words/Ireland_black_bear.txt")
+    df <- readtext:::getdocvarsFromFilenames(filenames, 
+                                             docvarnames = c("country", "color", "animal"))
+    expect_equal(df$animal,
+                 c("horse", "dog", "dragon", "bear"))
+    
+    expect_equal(names(df), c("country", "color", "animal"))
+    
+    expect_s3_class(df, "data.frame")
+})
+
+
+test_that("file_ext returns expected extensions", {
+    filenames <- c("~/tmp/documents/USA_blue_horse.txt",
+                   "~/tmp/documents/France_green_dog.csv",
+                   "~/tmp/documents/China_red_dragon.json",
+                   "~/tmp/spaced words/Ireland_black_bear.tar.gz") 
+    expect_equal(readtext:::file_ext(filenames),
+                 c("txt", "csv", "json", "gz"))
+})
