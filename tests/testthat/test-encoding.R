7f12eccbb98b0c8f48ab2fa2d8851f75a171accb
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-03-30 17:21:21 +0100

    Expand tests

--- tests/testthat/test-encoding.R
+++ tests/testthat/test-encoding.R
@@ -8,6 +8,19 @@ test_that("test encoding function on simple text files", {
         enc, 
         list(probably = "UTF-8", all = "UTF-8")
     )
+
+    expect_equal(
+        enc, 
+        list(probably = "UTF-8", all = "UTF-8")
+    )
+    
+    expect_message(
+        encoding(rt, verbose = TRUE),
+        "Probable encoding: UTF-8"
+    )
+
+    rt <- readtext(tempf, encoding = "UTF-8")
+    
 })
 
 test_that("test encoding function on simple text files, Russian 8-bit encoding", {
