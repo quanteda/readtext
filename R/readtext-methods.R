2c2db3a2951d74a0160a9616afbd05de7c7de667
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-04-28 07:03:21 +0100

    Using stringi::sub to handle UTF-8 substrings

--- R/readtext-methods.R
+++ R/readtext-methods.R
@@ -8,16 +8,17 @@
 #' @param ... not used here
 #' @importFrom utils head
 #' @importFrom tibble trunc_mat
+#' @importFrom stringi stri_sub
 #' @export
 print.readtext <- function(x, n = 6L, text_width = 10L, ...) {
     cat("readtext object consisting of ", nrow(x), 
         " document", ifelse(nrow(x) == 1, "", "s"), " and ", 
         ncol(x)-1, " docvar", ifelse((ncol(x)-1) == 1, "", "s"), 
         ".\n", sep="")
-    x$text <- paste0("\"", substr(x$text, 1, text_width), "\"...")
+    x$text <- paste0("\"", stringi::stri_sub(x$text, length = text_width), "\"...")
     x <- cbind(data.frame(doc_id = rownames(x), stringsAsFactors = FALSE), x)
     class(x) <- "data.frame"
-    print(tibble::trunc_mat(x, n = 6))
+    print(tibble::trunc_mat(x, n = n))
 }
 
 #' return only the texts from a readtext object
