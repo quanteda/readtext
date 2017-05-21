aca78b1cda3e12ead767fdfe87403223cfe31688
Adam Obeng <github@binaryeagle.com>
2017-05-21 18:06:52 +0100

    Pass correct path arg
    Signed-off-by: Adam Obeng <github@binaryeagle.com>

--- R/get-functions.R
+++ R/get-functions.R
@@ -242,7 +242,7 @@ get_excel <- function(f, text_field, ...) {
     }
 
     docs <- data.table::rbindlist(sheets, fill=TRUE)
-    text_field <- get_numeric_textfield(text_field, docs, path)
+    text_field <- get_numeric_textfield(text_field, docs, f)
 
     data.frame(text = docs[,text_field, with=F], docs[, -text_field, with=FALSE],
                stringsAsFactors = FALSE)
