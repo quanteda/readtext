a99b750bb55ab189754f4de261917bffe37142bf
Adam Obeng <github@binaryeagle.com>
2017-05-21 11:56:07 +0100

    Fix typo
    Signed-off-by: Adam Obeng <github@binaryeagle.com>

--- R/get-functions.R
+++ R/get-functions.R
@@ -250,7 +250,7 @@ get_excel <- function(f, text_field, ...) {
 
 
 get_ods <- function(f, text_field, ...) {
-    sheet_names <- readxl::ods_sheets(f)
+    sheet_names <- readODS::ods_sheets(f)
     sheets <- lapply(sheet_names, function(x, ...) {readODS::read_ODS(f, sheet=x, ...)})
 
     if (length(unique(sapply(sheets, ncol))) != 1) {
