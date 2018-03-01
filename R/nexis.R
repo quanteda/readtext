6dab0c59db76e359a7e430372094f98e568dfafb
Kohei Watanabe <watanabe.kohei@gmail.com>
2018-03-01 18:04:03 +0000

    Add nexis importer

---
+++ R/nexis.R
@@ -0,0 +1,174 @@
+#' extract texts and meta data from Nexis HTML files
+#'
+#' This extract headings, body texts and meta data (date, byline, length,
+#' secotion, edntion) from items in HTML files downloaded by the scraper.
+#' @param path either path to a HTML file or a directory that containe HTML files
+#' @param paragraph_separator a character to sperarate paragrahphs in body texts.
+#' @param language_date a character to specify langauge-dependent date format.
+#' @param raw_date return date of publication without parsing if \code{TRUE}.
+#' @export
+#' @examples
+#' irt <- import_nexis('tests/html/irish-times_1995-06-12_0001.html')
+#' afp <- import_nexis('tests/html/afp_2013-03-12_0501.html')
+#' gur <- import_nexis('tests/html/guardian_1986-01-01_0001.html')
+#' sun <- import_nexis('tests/html/sun_2000-11-01_0001.html')
+#' spg <- import_nexis('tests/html/spiegel_2012-02-01_0001.html', language_date = 'german')
+#' all <- import_nexis('tests/html', raw_date = TRUE)
+import_nexis <- function(path, paragraph_separator = '|', language_date = c('english', 'german'), raw_date = FALSE){
+
+    language_date <- match.arg(language_date)
+
+    if (dir.exists(path)) {
+        dir <- path
+        file <- list.files(dir, full.names = TRUE, recursive = TRUE)
+        data <- data.frame()
+        for(f in file){
+            #print(file)
+            if(stri_detect_regex(f, '\\.html$|\\.htm$|\\.xhtml$', ignore.case = TRUE)){
+                data <- rbind(data, import_html(f, paragraph_separator, language_date, raw_date))
+            }
+        }
+    } else if (file.exists(path)) {
+        data <- import_html(path, paragraph_separator, language_date, raw_date)
+    } else {
+        stop(path, " does not exist")
+    }
+    return(data)
+}
+
+#' extract texts and meta data from Nexis HTML files
+#'
+#' This extract headings, body texts and meta data (date, byline, length,
+#' secotion, edntion) from items in HTML files downloaded by the scraper.
+#' @param path either path to a HTML file or a directory that containe HTML files
+#' @param paragraph_separator a character to sperarate paragrahphs in body texts.
+#' @param language_date a character to specify langauge-dependent date format.
+#' @param raw_date return date of publication without parsing if \code{TRUE}.
+#' @param ... only to trap extra aguments
+#' @import stringi XML
+#' @examples
+#' irt <- readtext:::get_nexis_html('tests/data/nexis/irish-times_1995-06-12_0001.html')
+#' afp <- readtext:::get_nexis_html('tests/data/nexis/afp_2013-03-12_0501.html')
+#' gur <- readtext:::get_nexis_html('tests/data/nexis/guardian_1986-01-01_0001.html')
+#' sun <- readtext:::get_nexis_html('tests/data/nexis/sun_2000-11-01_0001.html')
+#' spg <- readtext:::get_nexis_html('tests/data/nexis/spiegel_2012-02-01_0001.html', language_date = 'german')
+#' 
+#' all <- readtext('tests/data/nexis', souce = 'nexis')
+#' all <- readtext('tests/data/nexis', souce = 'nexis')
+get_nexis_html <- function(path, paragraph_separator = '\n\n', 
+                           language_date = c('english', 'german'), raw_date = TRUE,
+                           verbosity, ...){
+    
+    language_date <- match.arg(language_date)
+
+    line <- readLines(path, warn = FALSE, encoding = "UTF-8")
+    html <- paste0(fix_html(line), collapse = "\n")
+
+    #Load as DOM object
+    dom <- htmlParse(html, encoding = "UTF-8")
+    data <- data.frame()
+    for(doc in getNodeSet(dom, '//doc')){
+        data <- rbind(data, extract_attrs(doc, paragraph_separator, language_date, raw_date))
+    }
+    colnames(data) <- c('pub', 'edition', 'date', 'byline', 'length', 'section', 'head', 'body')
+    data$file <- basename(path)
+
+    return(data)
+}
+
+#' @import stringi XML
+extract_attrs <- function(node, paragraph_separator, language_date, raw_date) {
+
+    attrs <- list(pub = '', edition = '', date = '', byline = '', length = '', section = '', head = '', body = '')
+
+    if (language_date == 'german') {
+        regex <- paste0(c('([0-9]{1,2})',
+                          '[. ]+(Januar|Februar|März|Maerz|April|Mai|Juni|Juli|August|September|Oktober|November|Dezember)',
+                          '[ ]+([0-9]{4})',
+                          '([ ]+(Montag|Dienstag|Mittwoch|Donnerstag|Freitag|Samstag|Sonntag))?',
+                          '([, ]+(.+))?'), collapse = '')
+    } else {
+        regex <- paste0(c('(January|February|March|April|May|June|July|August|September|October|November|December)',
+                          '[, ]+([0-9]{1,2})',
+                          '[, ]+([0-9]{4})',
+                          '([,; ]+(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday))?',
+                          '([, ]+(.+))?'), collapse = '')
+    }
+
+    n_max <- 0;
+    i <- 1
+    #print(node)
+    for(div in getNodeSet(node, './/div')){
+
+        str <- xmlValue(div, './/text()')
+        str <- clean_text(str)
+        n <- stri_length(str);
+        if (is.na(n)) next
+
+        #cat('----------------\n')
+        #cat(i, stri_trim(s), "\n")
+
+        if (i == 2) {
+            attrs$pub <- stri_trim(str)
+        } else if (i == 3) {
+            if (raw_date) {
+                attrs$date <- stri_trim(str)
+            } else {
+                m <- stri_match_first_regex(str, regex)
+                if (all(!is.na(m[1,2:4]))) {
+                    date <- paste0(m[1,2:4], collapse = ' ')
+                    if (language_date == 'german') {
+                        datetime <- stri_datetime_parse(date, 'd MMMM y', locale = 'de_DE')
+                    } else {
+                        datetime <- stri_datetime_parse(date, 'MMMM d y', locale = 'en_EN')
+                    }
+                    attrs$date <- stri_datetime_format(datetime, 'yyyy-MM-dd')
+                }
+                if (!is.na(m[1,8])) {
+                    attrs$edition <- stri_trim(m[1,8])
+                }
+            }
+        } else if (i == 4) {
+            attrs$head <- stri_trim(str)
+        } else if (i >= 5) {
+            if (stri_detect_regex(str, "^BYLINE: ")) {
+                attrs$byline = stri_trim(stri_replace_first_regex(str, "^BYLINE: ", ''))
+            } else if (stri_detect_regex(str, "^SECTION: ")) {
+                attrs$section = stri_trim(stri_replace_first_regex(str, "^SECTION: ", ''));
+            } else if (stri_detect_regex(str, "^LENGTH: ")) {
+                attrs$length = stri_trim(stri_replace_all_regex(str, "[^0-9]", ''))
+            } else if (!is.null(attrs$length) && n > n_max &&
+                       !stri_detect_regex(str, "^(BYLINE|URL|LOAD-DATE|LANGUAGE|GRAPHIC|PUBLICATION-TYPE|JOURNAL-CODE): ")){
+                ps <- getNodeSet(div, './/p')
+                p <- sapply(ps, xmlValue)
+                attrs$body <- stri_trim(paste0(p, collapse = paste0(' ', paragraph_separator, ' ')))
+                n_max = n
+            }
+        }
+        i <- i + 1
+    }
+    if (attrs$pub[1] == '' || is.na(attrs$pub[1])) warning('Failed to extract publication name')
+    if (attrs$date[1] == '' || is.na(attrs$date[1])) warning('Failed to extract date')
+    if (attrs$head[1] == '' || is.na(attrs$head[1])) warning('Failed to extract heading')
+    if (attrs$body[1] == '' || is.na(attrs$body[1])) warning('Failed to extract body text')
+    return(as.data.frame(attrs, stringsAsFactors = FALSE))
+}
+
+#' @import stringi
+clean_text <- function(str) {
+    str <- stri_replace_all_regex(str, '[[:^print:]]', ' ');
+    str <- stri_replace_all_regex(str, "[\\r\\n\\t]", ' ')
+    str <- stri_replace_all_regex(str, "\\s\\s+", ' ')
+    str <- stri_trim(str);
+    return(str)
+}
+
+#' @import stringi
+fix_html <- function(line){
+    line <- stri_replace_all_fixed(line, '<!-- Hide XML section from browser', '');
+    line <- stri_replace_all_fixed(line, '<DOCFULL> -->', '<DOCFULL>');
+    line <- stri_replace_all_fixed(line, '</DOC> -->', '</DOC>');
+    line <- stri_replace_all_fixed(line, '<BR>', '<BR> ');
+    return(line)
+}
+
