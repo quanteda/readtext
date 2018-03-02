eea92872383f65049e79b4b95f37f87487aeeeab
Kohei Watanabe <watanabe.kohei@gmail.com>
2018-03-02 15:24:28 +0000

    Modernized utility functions

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,8 @@
+# Returns supported file extensions
+extensions <- function() {
+    c('csv', 'txt', 'json', 'zip', 'gz', 'tar', 'xml', 'tab', 
+      'tsv', 'html', 'pdf', 'docx', 'doc', 'xls', 'xlsx', 'ods')
+}
 
 ## from the tools package
 file_ext <- function (x) {
@@ -6,32 +11,33 @@ file_ext <- function (x) {
 }
 
 #' @importFrom tools file_path_sans_ext
-getdocvarsFromFilenames <- function(fnames, dvsep="_", docvarnames=NULL, include_path=FALSE) {
-    snames <- fnames
-    if (include_path==FALSE) {
-        snames <- tools::file_path_sans_ext(basename(snames))
-    }
-    parts <- strsplit(snames, dvsep)
+get_docvars_filenames <- function(path, dvsep = "_", docvarnames = NULL, 
+                                  include_path = FALSE, verbosity) {
     
-    if (!all(sapply(parts,function(x) identical(length(x), length(parts[[1]])))))
+    if (include_path) {
+        name <- path
+    } else {
+        name <- tools::file_path_sans_ext(basename(path))
+    }
+    parts <- strsplit(name, dvsep)
+    if (any(length(parts[[1]]) != lengths(parts)))
         stop("Filename elements are not equal in length.")
     
-    dvars <-  data.frame(matrix(unlist(parts), nrow=length(parts), byrow=TRUE), 
-                         stringsAsFactors=FALSE)
+    docvar <-  data.frame(matrix(unlist(parts), nrow = length(parts), byrow = TRUE), 
+                         stringsAsFactors = FALSE)
     # assign default names in any case
-    names(dvars) <- paste("docvar", 1:ncol(dvars), sep="")  
+    names(docvar) <- paste("docvar", seq(ncol(docvar)), sep="")  
     if (!is.null(docvarnames)) {
-        names(dvars)[1:length(docvarnames)] <- docvarnames
-        if (length(docvarnames) != ncol(dvars)) {
-            if (getOption("readtext_verbosity") >= 1) { 
-                warning("Fewer docnames supplied than existing docvars - last ",
-                        ncol(dvars) - length(docvarnames), " docvar",
-                        ifelse((ncol(dvars) - length(docvarnames))==1, "", "s"),
-                        " given generic names.")
-            }
+        names(docvar)[seq_along(docvarnames)] <- docvarnames
+        if (verbosity >= 1 && length(docvarnames) != ncol(docvar)) {
+            warning("Fewer docnames supplied than existing docvars - last ",
+                    ncol(docvar) - length(docvarnames), " docvar",
+                    if (ncol(docvar) - length(docvarnames) == 1) "" else "s",
+                    " given generic names.")
+            
         }
     }
-    dvars
+    return(docvar)
 }
 
 # @rdname mktemp
@@ -49,7 +55,7 @@ mktemp <- function(prefix = "tmp.", base_path = NULL, directory = FALSE) {
     filename <- paste0(prefix, filename)
     filename <- file.path(base_path, filename)
     while (file.exists(filename) || dir.exists(filename)) {
-        filename <- paste0(sample(alphanumeric, 10, replace = TRUE), collapse='')
+        filename <- paste0(sample(alphanumeric, 10, replace = TRUE), collapse = '')
         filename <- paste0(prefix, filename)
         filename <- file.path(base_path, filename)
     }
@@ -64,36 +70,34 @@ mktemp <- function(prefix = "tmp.", base_path = NULL, directory = FALSE) {
 }
 
 
-downloadRemote <- function (i, ignoreMissing) {
+downloadRemote <- function (i, ignore_missing) {
     # First, check that this is not a URL with an unsupported scheme
     scheme <- stri_match(i, regex='^([a-z][a-z+.-]*):')[, 2]
-    if (!(scheme %in% c('http', 'https', 'ftp'))) {
+    if (!scheme %in% c('http', 'https', 'ftp'))
         stop(paste('Unsupported URL scheme', scheme))
-    }
     
     # If this is a supported-scheme remote URL
-    extension <- tools::file_ext(i)
-    if (!(extension %in% SUPPORTED_FILETYPES)) {
+    ext <- tools::file_ext(i)
+    if (!ext %in% extensions())
         stop('Remote URL does not end in known extension. Please download the file manually.')
-    }
-    localfile <- file.path(mktemp(directory=T), basename(i))
+    
+    localfile <- file.path(mktemp(directory = TRUE), basename(i))
     r <- httr::GET(i, httr::write_disk(localfile))
-    if (ignoreMissing) {
+    if (ignore_missing) {
         httr::warn_for_status(r)
-    }
-    else {
+    } else {
         httr::stop_for_status(r)
     }
     localfile
 }
 
-#  The implementation of listMatchingFiles and listMatchingFile might seem very
+#  The implementation of list_files and list_file might seem very
 #  complex, but it was arrived at after a lot of toil. The main design decision
 #  made here is that the user should be able to pass many different types of
-#  string to listMatchingFiles and get a consistent result: a list of local
+#  string to list_files and get a consistent result: a list of local
 #  filenames. (One additional wrinkle is that there are two functions,
-#  listMatchingFiles and listMatchingFile. This is to ensure that
-#  listMatchingFile is only ever called with a length 1 argument, even though it
+#  list_files and list_file. This is to ensure that
+#  list_file is only ever called with a length 1 argument, even though it
 #  can return multiple filenames. For the purposes of this explanation, this
 #  distinction is elided).
 #  There are four possible types of values for x
@@ -101,7 +105,7 @@ downloadRemote <- function (i, ignoreMissing) {
 #     - a remote URL
 #     - a glob pattern
 #     - a vector of some combination of the above
-#  listMatchingFiles has a recursive design, because  some of these arguments
+#  list_files has a recursive design, because  some of these arguments
 #  can resolve to arguments which need further processing: e.g. a remote URL
 #  could resolve to a zip file which needs to be extracted. The termination
 #  condition for the recursion is when the argument passed is a local filepath
@@ -121,104 +125,90 @@ downloadRemote <- function (i, ignoreMissing) {
 #  doesn't work either. We also can't test whether a pattern is a regular file
 #  by looking at the extension, because '/path/to/*.zip' is a glob expression
 #  with a 'zip' extension.
-listMatchingFiles <- function(x, ignoreMissing = FALSE, lastRound = FALSE) {
+list_files <- function(x, ignore_missing = FALSE, last_round = FALSE) {
 
-    if (!(ignoreMissing || (length(x) > 0))) {
+    if (!(ignore_missing || (length(x) > 0)))
         stop("File '", x, "' does not exist.")
-    }
     
-    matchingFiles <- unlist(
-        lapply(x, function (x) listMatchingFile(
-            x,
-            ignoreMissing=ignoreMissing,
-            lastRound=lastRound)
-        )
-    )
+    file <- unlist(lapply(x, function (x) list_file(x, ignore_missing, last_round)))
     
-    if (is.null(matchingFiles)) return(character(0))
-    
-    matchingFiles
+    if (is.null(file)) 
+        return(character(0))
+    sort(file)
 }
 
-extractArchive <- function(i, ignoreMissing) {
-    if (!(ignoreMissing || file.exists(i)))
+extract_archive <- function(i, ignore_missing) {
+    if (!(ignore_missing || file.exists(i)))
         stop("File '", i, "' does not exist.")
     
-    td <- mktemp(directory=T)
-    if (tools::file_ext(i) == 'zip' ||
-        tools::file_ext(i) == 'docx'
-        )
+    td <- mktemp(directory = TRUE)
+    ext <- tools::file_ext(i)
+    if (ext %in% c('zip', 'docx')) {
         utils::unzip(i, exdir = td)
-    else if ( tools::file_ext(i) == 'gz' ||
-              tools::file_ext(i) == 'tar' ||
-              tools::file_ext(i) == 'bz' )
+    } else if (ext %in% c('gz', 'tar', 'bz')) {
         utils::untar(i, exdir = td)
-    else 
+    } else { 
         stop("Archive extension '", tools::file_ext(i), "' unrecognised.")
-    
+    }
     # Create a glob that matches all the files in the archive
     file.path(td, '*')
 }
 
 #' @import stringi
-listMatchingFile <- function(x, ignoreMissing, lastRound) {
+list_file <- function(x, ignore_missing, last_round) {
     filenames <- c()
     #  Remove 'file' scheme
     i <- stri_replace_first_regex(x, "^file://", "")
-    
     scheme <- stri_match_first_regex(i, "^([A-Za-z][A-Za-z0-9+.-]+)://")[, 2]
     
     # If not a URL (or a file:// URL) , treat it as a local file
     if (!is.na(scheme)) {
-        if (getOption("readtext_verbosity") >=3 ) message(', reading remote file', appendLF = FALSE)
+        if (getOption("readtext_verbosity") >=3 ) 
+            message(', reading remote file', appendLF = FALSE)
         #  If there is a non-'file' scheme, treat it as remote
-        localfile <- downloadRemote(i, ignoreMissing=ignoreMissing)
-        return(listMatchingFiles(localfile, ignoreMissing=ignoreMissing))
+        localfile <- downloadRemote(i, ignore_missing = ignore_missing)
+        return(list_files(localfile, ignore_missing = ignore_missing))
     }
     
     # Now, special local files
-    if (tools::file_ext(i) == 'zip' ||
-        tools::file_ext(i) == 'gz' ||
-        tools::file_ext(i) == 'tar' ||
-        tools::file_ext(i) == 'bz' 
-    ) {
+    ext <- tools::file_ext(i)
+    if (ext %in% c('zip', 'gz', 'tar', 'bz')) {
         if (getOption("readtext_verbosity") >= 3) 
-            message(", unpacking .", tools::file_ext(i), " archive", appendLF = FALSE)
-        archiveFiles <- extractArchive(i, ignoreMissing=ignoreMissing)
-        return(listMatchingFiles(archiveFiles, ignoreMissing=ignoreMissing))
+            message(", unpacking .", ext, " archive", appendLF = FALSE)
+        archiveFiles <- extract_archive(i, ignore_missing = ignore_missing)
+        return(list_files(archiveFiles, ignore_missing = ignore_missing))
     }
     
     #  At this point, it may be a simple local file or a glob pattern, but as
     #  above, we have no way of telling a priori whether this is the case
-    if (lastRound) {
+    if (last_round) {
         #  We get to this point if the path wasn't to some file that needed
         #  special treatment (zip, remote, etc.) and it was treated as a glob
         #  pattern, which means that it is definitely not a glob pattern this
         #  time
-        if (dir.exists(i)) {
-                return(listMatchingFiles(ignoreMissing=ignoreMissing, file.path(i, '*')))
-        }
-        if (!(ignoreMissing || file.exists(i))) {
-                stop("File '", i, "' does not exist.")
-        }
+        if (dir.exists(i))
+            return(list_files(ignore_missing=ignore_missing, file.path(i, '*')))
+        
+        if (!(ignore_missing || file.exists(i)))
+            stop("File '", i, "' does not exist.")
+        
         if (getOption("readtext_verbosity") >= 3) 
             message("... reading (", tools::file_ext(i), ") file: ", basename(i))
         return(i)
-    }
-    else {
+    } else {
         #  If it wasn't a glob pattern last time, then it may be this time
         if (getOption("readtext_verbosity") >= 3) message(", using glob pattern")
         i <- Sys.glob(i)
         return(
-            listMatchingFiles(i, ignoreMissing = ignoreMissing, lastRound = T)
+            list_files(i, ignore_missing = ignore_missing, last_round = TRUE)
         )
     }
     
 }
 
 #' Return basenames that are unique
-#' @param x file pathes
-#' @param path_only if \code{TRUE}, only return the unique part of the path
+#' @param x character vector; file pathes
+#' @param path_only logical; if \code{TRUE}, only return the unique part of the path
 #' @keywords internal
 #' @examples
 #' files <- c("../data/glob/subdir1/test.txt", "../data/glob/subdir2/test.txt")
@@ -239,12 +229,16 @@ basename_unique <- function(x, path_only = FALSE) {
         index <- seq(min(which(!is_uniform)), max(which(!is_uniform)))
     }
     temp <- temp[index,,drop = FALSE]
-    sapply(temp, paste0, collapse = "/", USE.NAMES = FALSE)
+    unlist(lapply(temp, paste0, collapse = "/"), use.names = FALSE)
 }
 
 
-## impute the variable types, just like read.csv
-## needed when the files read in tend to make everything character 
+#' Detect and set variable types automatically
+#' 
+#' Detect and set variable types in a similar way as \code{read.csv()} does.
+#' Should be used when imported data.frame is all characters.
+#' @param x data.frame; colums are all characters vectors
+#' @keywords internal
 impute_types <- function(x) {
     if (nrow(x) == 0) return(x)
     # Impute types of columns, just like read.table
@@ -252,6 +246,7 @@ impute_types <- function(x) {
     # And convert columns which have been made into factors into strings
     is_factor <- sapply(x, is.factor)
     x[is_factor] <- lapply(x[is_factor], as.character)
+    x <- data.frame(x, stringsAsFactors = FALSE)
     return(x)
 }
 
@@ -262,16 +257,31 @@ is_probably_xpath <- function(x) {
     )
 }
 
-sort_fields <- function(x, text_field, impute_types = FALSE) {
+#' Move text to the first column and set types to document variables
+#' 
+#' @param x data.frame; contains texts and document variables
+#' @param paht character; file path from which \code{x} is created; only use in error message
+#' @param text_filed numeric or character; indicate position of a text column in x
+#' @param inpute_types logical; if \code{TRUE}, set types of variables automatically
+#' @keywords internal
+sort_fields <- function(x, path, text_field, impute_types = TRUE) {
+    x <- as.data.frame(x)
     index <- seq(ncol(x))
     flag <- FALSE
+    text_field <- text_field[1]
     if (is.numeric(text_field)) {
         flag <- index == text_field
+        if (sum(flag) == 0)
+            stop(sprintf("There is no %dth field in file %s.", text_field, path))
     } else if (is.character(text_field)) {
         flag <- names(x) == text_field
+        if (sum(flag) == 0) {
+            stop(sprintf("There is no field called %s in file %s.", text_field, path))
+        } else if (sum(flag) > 1) {
+            stop(sprintf("There is more than one field called %s in file %s.", text_field, path))
+        }
     }
-    stopifnot(sum(flag) == 1)
-    x <- x[c(index[flag], index[!flag])]
+    x <- x[,c(index[flag], index[!flag])]
     names(x)[1] <- 'text' 
     if (impute_types) {
         return(impute_types(x))
